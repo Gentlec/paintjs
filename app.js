85cf703b3b1d71267ba261486e81e77f0e424153
Gentlec <65644941+Gentlec@users.noreply.github.com>
2021-05-31 21:15:34 +0900

    Can choose the color

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
@@ -27,9 +28,19 @@ const onMouseMove = (event) => {
   }
 };
 
+const handleColorClick = (event) => {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  return;
+};
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
